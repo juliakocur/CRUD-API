8ca938c2a8d4db4ec6a91e2914424169033497ef
Julia Kocur <juliakocur@mail.com>
2025-05-08 11:24:20 +0200

    refactor: add module router

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,17 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
-import { getAllUsers, getUserById } from './controllers/users';
 import dotenv from 'dotenv';
-import { parse } from 'url';
+import { userRouter } from './routes/router';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  const parsedUrl = parse(req.url || '', true);
-  const { pathname: url } = parsedUrl;
-
-  if (req.method === 'GET' && url === '/api/users') {
-    return getAllUsers(req, res);
+  if (!userRouter(req, res)) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ message: 'Not found' }));
   }
-
-  const userId = url?.match(/^\/api\/users\/([0-9a-fA-F-]{36})$/);
-    if (req.method === 'GET' && userId) {
-    const id = userId[1];
-    return getUserById(req, res, id);
-  }
-
-  res.statusCode = 404;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Not found' }));
 });
 
 server.listen(PORT, () => {
---
+++ src/routes/router.ts
@@ -0,0 +1,22 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { getAllUsers, getUserById } from '../controllers/users';
+import { parse } from 'url';
+
+export const userRouter = (req: IncomingMessage, res: ServerResponse): boolean => {
+  const parsedUrl = parse(req.url || '', true);
+  const { pathname: url } = parsedUrl;
+
+  if (req.method === 'GET' && url === '/api/users') {
+    getAllUsers(req, res);
+    return true;
+  }
+
+  const userIdMatch = url?.match(/^\/api\/users\/([0-9a-fA-F-]{36})$/);
+  if (req.method === 'GET' && userIdMatch) {
+    const id = userIdMatch[1];
+    getUserById(req, res, id);
+    return true;
+  }
+
+  return false;
+};
